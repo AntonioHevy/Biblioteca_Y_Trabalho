6942fb150d6373fdd1836aba696379c33cd09073
AntonioHevy <amegaxd1234@gmail.com>
2026-06-03 01:57:20 -0300

    Delete Node-API-main directory

--- Node-API-main/src/erros/naoEncontrado.js
+++
@@ -1,8 +0,0 @@
-import ErroBase from "./erroBase.js";
-
-class naoEncontrado extends ErroBase {
-  constructor(mensagem = "Pagina não encontrada") {
-    super(mensagem, 404);
-  }
-}
-export default naoEncontrado;
